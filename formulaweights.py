e90aac885798955fbf96b6a12cdde542a8ad5ebe
amaheshwari25 <arya.maheshwari@gmail.com>
2018-09-15 09:29:16 -0700

    Fix element count bug, move count input to be before element symbol

--- formulaweights.py
+++ formulaweights.py
@@ -9,7 +9,7 @@ ptable = requests.get('https://ptable.com/')
 soup = bs(ptable.text, 'html.parser')
 
 print("format: separate each element with a space, "
-      "put count of each element before that element (no space), "
+      "put count of each element after that element (no space), "
       "type DONE (no spaces!) to exit")
 
 elements = input("input compound?")
@@ -19,11 +19,15 @@ while not elements == 'DONE':
 
     weightSum = 0
     for elem in elemlist:
-        numString = "1"
-        while ord(elem[0]) < 65:
-            numString += elem[0]
+        elemSymb = ""
+        while len(elem) > 0 and ord(elem[0]) >= 65:
+            elemSymb += elem[0]
             elem = elem[1:]
-        elembox = soup.find('acronym', text=elem)
+        if elem == "":
+            numString = "1"
+        else:
+            numString = elem
+        elembox = soup.find('acronym', text=elemSymb)
         weight=elembox.findNext('i').text
         print("atomic weight: "+elembox.text+":"+weight)
         weightSum += float(weight)*int(numString)
